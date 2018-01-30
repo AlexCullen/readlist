79f4de3fac4a6054c08a5f4c148f859acd888f4d
cpzh <maseratipp@gmail.com>
2018-01-30 23:18:21 +0800

    spring boot 练习第一天

---
+++ src/main/java/com/example/readlist/ReadlistApplication.java
@@ -0,0 +1,11 @@
+package com.example.readlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadlistApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReadlistApplication.class, args);
+    }
+}
